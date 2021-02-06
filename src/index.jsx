9debcf126f43f8d896beb2ded6189f210ec5bbbc
marsoon <asroq98@gmail.com>
2021-02-06 16:29:56 +0900

    Add HashRouter

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { HashRouter } from 'react-router-dom';
 import App from './components/App';
 import "./styles.css";
 
 ReactDOM.render(
-  <React.StrictMode>
+  <HashRouter>
+    <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
+  </HashRouter>,
   document.getElementById('root')
 );
