3e51d342bc00ac023dcef9555da0061c0061e8f4
marsoon <asroq98@gmail.com>
2021-02-08 14:33:44 +0900

    i really don't knwo why it doesn't work

--- src/index.jsx
+++ src/index.jsx
@@ -5,10 +5,8 @@ import App from './components/App';
 import "./styles.css";
 
 ReactDOM.render(
-  <HashRouter>
     <React.StrictMode>
     <App />
-  </React.StrictMode>
-  </HashRouter>,
+  </React.StrictMode>,
   document.getElementById('root')
 );
