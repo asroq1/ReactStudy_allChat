b1b0298a3941030a1ec944b06ccfbb3887b0fdef
marsoon <asroq98@gmail.com>
2021-02-11 19:21:58 +0900

    fixed Index.html

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
+import App from 'components/App';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App';
-import "./styles.css";
-
+import './styles.css';
 ReactDOM.render(
     <React.StrictMode>
     <App />
