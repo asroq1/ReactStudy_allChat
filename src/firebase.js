0b1840dc907c9eab0499c0a766c2f7fbb52c09e4
marsoon <asroq98@gmail.com>
2021-02-11 18:37:52 +0900

    testing...

--- src/firebase.js
+++ src/firebase.js
@@ -2,6 +2,7 @@ import firebase from 'firebase';
 import "firebase/auth";
 import "firebase/firestore";
 import "firebase/storage";
+
 const firebaseConfig = {
     apiKey: process.env.REACT_APP_API_KEY,
     authDomain: process.env.REACT_APP_AUTH_DOMAIN,
@@ -15,9 +16,6 @@ const firebaseConfig = {
   firebase.initializeApp(firebaseConfig);
 
   export const firebaseInstance = firebase; 
-  
   export const authService = firebase.auth();
-
   export const dbService = firebase.firestore();
-
   export const storageService = firebase.storage();
