10fdc9d899920f4dd6f2ff72cfd62640a1df93a7
marsoon <asroq98@gmail.com>
2021-02-11 19:53:37 +0900

    i'm so sad

--- src/firebase.js
+++ src/firebase.js
@@ -3,7 +3,7 @@ import "firebase/auth";
 import "firebase/firestore";
 import "firebase/storage";
 
-const firebaseConfig = {
+const firebaseConfig={
   apiKey: process.env.REACT_APP_API_KEY,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
   databaseURL: process.env.REACT_APP_DATABASE_URL,
